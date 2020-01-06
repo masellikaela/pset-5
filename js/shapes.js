640910aef057821a73331fbbd50f8c6769d45328
Kaela Maselli <ksmaselli@gmail.com>
2020-01-05 22:02:06 -0500

    Update shapes.js

--- js/shapes.js
+++ js/shapes.js
@@ -6,18 +6,6 @@ window.onload = function() {
     document.getElementById("smile-sample").onclick = drawFaceStaff;
     document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
 
-    // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
-    //
-    // document.getElementById("some-id")   <-- you need to give each button a unique ID
-    //                                          and access it in this manner
-    //
-    // onclick is an event that is fired when you click something (in our case, a button).
-    // when we give onclick a value, we're telling JavaScript what to do when we click the button.
-    // you should set onclick equal to your function names (i.e., sayHello).
-    //
-    // there are six event listeners being added for the staff solutions. you'll have an
-    // equivalent set of six event listeners for your solutions. the first one is done for you.
-
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
     document.getElementById("color").onclick = drawColoredRectangle;
@@ -59,57 +47,45 @@ const sayHello = function() {
      const ctx = canvas.getContext('2d');
      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-     let width, height, x, y;
-     do {
-         width = prompt("Width: ")
-         if (width == null) { // do i need to break bc it still asks questions even if you press cancel???
-           break;
-         }
-         console.log("next");
-         height = prompt("Height: ")
-         if (height == null) {
-           break;
-         }
-         x = prompt("X: ")
-         if (x == null) {
-           break;
-         }
-         y = prompt("Y: ")
-         if (y == null) {
-           break;// do i need any of these if statements??
-         }
-         if (width > 1024 || width < 1) {
-           alert("Your width must be between 1 and 1024.")
-         }
-         if (height > 512 || height < 1) {
-           alert("Your height must be between 1 and 512.")
-         }
-         if (x < 1 || x > 1024) {
-           alert("Your x-coordinate must be between 1 and 1024.")
-         }
-         if (y < 1 || y > 512) {
-           alert("Your y-coordinate must be between 1 and 512.")
-         }
-         if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {
-           alert("One of your values is not a number.")
-         }
-      } while (isNaN(width) ||
-             isNaN(height) ||
-             isNaN(x) ||
-             isNaN(y) ||
-             width > 1024 ||
-             width < 1 ||
-             height > 512 ||
-             height < 1 ||
-             x < 1 ||
-             x > 1024 ||
-             y < 1 ||
-             y > 512);
+     var width = 0;
+     var height = 0;
+     var x = 0;
+     var y = 0;
+   do {
+     var width = prompt("Width: ")
+     var height = prompt("Height: ")
+     var x = prompt("X: ")
+     var y = prompt("Y: ")
+     if (width == null || height == null || x == null || y == null) {
+       break;
+     }
+     if (width > 1024 || width < 1) {
+       alert("Your width must be between 1 and 1024.")
+     }
+     else if (height > 512 || height < 1) {
+       alert("Your height must be between 1 and 512.")
+     }
+     else if (x < 1 || x > 1024) {
+       alert("Your x-coordinate must be between 1 and 1024.")
+     }
+     else if (y < 1 || y > 512) {
+       alert("Your y-coordinate must be between 1 and 512.")
+     }
+     else if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {
+       alert("One of your values is not a number.")
+     }
+     else if (Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512) {
+       alert("Your rectangle won't fit on the canvas.")
+     }
+   } while (width > 1024 || width < 1 || height > 512 || height < 1 || x < 1 || x > 1024 || y < 1 || y > 512 || isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y) || Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512)
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeRect(x, y, width, height);
+   if (!(width == null) && !(height == null) && !(x == null) && !(y == null)) {
+     ctx.beginPath();
+     ctx.rect(x, y, width, height);
+     ctx.closePath();
+     ctx.stroke();
+   }
 };
-
 /*
  * Exercise 3.
  */
